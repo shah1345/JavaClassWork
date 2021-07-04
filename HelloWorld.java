1e66c24891da2c26472c4b4c89fbdc5232a26aa0
Shah Newaz <34304584+shah1345@users.noreply.github.com>
2021-07-04 16:26:31 +0600

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,13 @@
 class HelloWorld { //In Java, every application begins with a class definition. In the program, HelloWorld is the name of the class, and the class definition is:
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
+        
+        
+         for(int i=1;i<=500;i++){
+            if(i % 10 ==3 || i % 10 ==7)
+             System.out.println(i); 
+            
+        }
+        
     }
 }
